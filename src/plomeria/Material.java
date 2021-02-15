a449b75338f4b0fe57146bcbd26859728811da92
Mariano Tugnarelli <mariano.tugnarelli@grupoesfera.com.ar>
2021-02-15 01:50:27 -0300

    Código del enunciado

---
+++ src/plomeria/Material.java
@@ -0,0 +1,13 @@
+package plomeria;
+
+public enum Material {
+
+    LADRILLO,
+    HORMIGON,
+    CANIO_VERTICAL,
+    CANIO_HORIZONTAL,
+    CANIO_ARRIBA_DERECHA,
+    CANIO_ARRIBA_IZQUIERDA,
+    CANIO_ABAJO_DERECHA,
+    CANIO_ABAJO_IZQUIERDA
+}
---
+++ src/plomeria/Plomero.java
@@ -0,0 +1,21 @@
+package plomeria;
+
+public class Plomero {
+	
+	/**
+     * pre : pared tiene:
+     *          - un elemento Material.CANIO_HORIZONTAL en la primer columna 
+     *          - un elemento Material.CANIO_HORIZONTAL en la última columna
+
+     *          
+     * post: cambia pared para que tenga una cañería que conecte todos los caños de los extremos, 
+     *       solo modificando aquellas posiciones en las que exista un elemento Material.LADRILLO.
+     * 
+     * @param pared : arreglo regular, donde la primera dimensión corresponde 
+     *                con el alto (fila) y la segunda con el ancho (columna). 
+     *                
+     */
+    public void construirCanieria(Material[][] pared) {
+    	
+    }
+}
